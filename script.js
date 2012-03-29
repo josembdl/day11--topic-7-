7f1372a8eab191b42330a6968b3856b0a56c1f1e
Jose Burgardt <joseburg89@hotmail.com>
2012-03-29 15:09:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+ 
+    if (screen.width >= 1200){
+        $("article").css({
+            "float": "left",
+            "display": "block",
+            "width": "48%",
+            "margin-left": "10px"
+        });
+    }
+    
+});
